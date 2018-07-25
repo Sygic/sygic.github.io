ca165791a6805b734d7976c87cc5f1e38b07c6f7
Tomas Cicman <tcicman@sygic.com>
2018-07-25 12:32:23 +0200

    simplify displayed result

--- reverse-geocode-v1-demo/script.js
+++ reverse-geocode-v1-demo/script.js
@@ -1,6 +1,6 @@
 var apiKey = getApiKey();
 var map;
-var markersGroup;
+var popup;
 
 window.onload = function () {
 
@@ -9,7 +9,7 @@ window.onload = function () {
 
     map = L.map("map", {
         center: [lat, lon],
-        zoom: 15
+        zoom: 18
     });
 
     var sygicTileLayer = L.TileLayer.sygic(apiKey);
@@ -18,17 +18,16 @@ window.onload = function () {
     var contextMenu = L.popup();
 
     map.on('contextmenu',(e) => {
-        contextMenu.setContent('<button onclick="whatIsHere(' + e.latlng.lat + ',' + e.latlng.lng + ')">What\'s here?</button>').setLatLng(e.latlng).openOn(map);
+        contextMenu.setContent('<a href onclick="whatIsHere(' + e.latlng.lat + ',' + e.latlng.lng + '); return false;">What\'s here?</a>').setLatLng(e.latlng).openOn(map);
     });
 
     whatIsHere(lat,lon);
 }
 
 function whatIsHere (lat, lon) {
-    var markers = [];
 
-    if (markersGroup != null)
-        map.removeLayer(markersGroup);
+    if (popup != null)
+        map.removeLayer(popup);
 
     $.get('https://geocoding.api.sygic.com/v1/api/reversegeocode?location=' + lat + ',' + lon + '&key=' + apiKey).done(function (response) {
         if (response.results.length > 0) {
@@ -37,73 +36,34 @@ function whatIsHere (lat, lon) {
                 return result.type === "address"
             })
 
-            if (results.length == 0) {
-                results = response.results.filter(result => {
-                    return result.type === "road"
+            if(results.length > 1){
+                results = results.filter(l => {
+                    return l.location_type === "interpolated"
                 });
+            }
+            else {
                 if (results.length == 0) {
                     results = response.results.filter(result => {
-                        return result.type === "locality"
+                        return result.type === "road" && result.components[0].value != "(Unnamed Road)"
                     });
+                    if (results.length == 0) {
+                        results = response.results.filter(result => {
+                            return result.type === "locality"
+                        });
+                    }
                 }
             }
 
             results.forEach(function(element){
-                var locationLat = element.location.lat;
-                var locationLon = element.location.lon;
+                var formattedResult =  element.formatted_result;
 
-                // location type can be:
-                //                 // for address result type: "exact" or "interpolated"
-                //                 // for road result type: "closest point"
-                //                 // for locality result type: "centroid"
-                var locationType = humanize(element.location_type);
-
-                var formattedResult = locationType + ":<br>" + element.formatted_result;
-                var locationMarker;
-
-                if(element.type === "road"){
-                    locationMarker = createDonutMarker(locationLat, locationLon, formattedResult);
-                }
-                else{
-                    locationMarker = L.marker([locationLat, locationLon])
-                    locationMarker.bindPopup(formattedResult, {autoClose: false});
-                }
-
-                markers.push(locationMarker);
-            });
-
-            markersGroup = L.featureGroup(markers).addTo(map);
-
-            var bounds = markersGroup.getBounds();
-            map.fitBounds(bounds);
-
-            markersGroup.eachLayer(function (layer) {
-                layer.openPopup();
+                popup = L.popup()
+                    .setLatLng([lat,lon])
+                    .setContent(formattedResult)
+                    .openOn(map);
             });
         } else {
             alert("NO_RESULTS");
         }
     });
-}
-
-function humanize(str) {
-    var frags = str.split('_');
-    for (i=0; i<frags.length; i++) {
-        frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
-    }
-    return frags.join(" ");
-}
-
-function createDonutMarker(lat, lon, description) {
-    var options = {
-        borderColor: "#1e88e5",
-        iconShape: 'doughnut',
-        iconSize: [8, 8],
-        iconAnchor: [7, 5],
-        borderWidth: 2,
-    };
-    var marker = new L.marker([lat, lon], {icon: L.BeautifyIcon.icon(options)});
-    marker.bindPopup(description, {autoClose: false});
-
-    return marker;
 }
