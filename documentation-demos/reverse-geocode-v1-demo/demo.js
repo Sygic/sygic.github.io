191fd187eae737ba9a6e3331b5791954b4d87185
Tomas Cicman <tcicman@sygic.com>
2018-07-26 09:50:14 +0200

    fix rev geo v1

--- documentation-demos/reverse-geocode-v1-demo/demo.js
+++ documentation-demos/reverse-geocode-v1-demo/demo.js
@@ -1,11 +1,15 @@
-var apiKey = getApiKey();
-var map;
-var popup;
-
 window.onload = function () {
+    var apiKey = prompt("Please ender an API key valid for TomTom maps.");
 
-    var lat = 48.224158;
-    var lon = 16.988369;
+    if (apiKey == null || apiKey == "") {
+        location.reload();
+    }
+
+    var map;
+    var popup;
+
+    var lat = 51.50105925961465;
+    var lon = -0.12856364250183108;
 
     map = L.map("map", {
         center: [lat, lon],
@@ -21,49 +25,49 @@ window.onload = function () {
         contextMenu.setContent('<a href onclick="whatIsHere(' + e.latlng.lat + ',' + e.latlng.lng + '); return false;">What\'s here?</a>').setLatLng(e.latlng).openOn(map);
     });
 
-    whatIsHere(lat,lon);
-}
+    function whatIsHere (lat, lon) {
 
-function whatIsHere (lat, lon) {
+        if (popup != null)
+            map.removeLayer(popup);
 
-    if (popup != null)
-        map.removeLayer(popup);
+        $.get('https://geocoding.api.sygic.com/v1/api/reversegeocode?location=' + lat + ',' + lon + '&key=' + apiKey).done(function (response) {
+            if (response.results.length > 0) {
 
-    $.get('https://geocoding.api.sygic.com/v1/api/reversegeocode?location=' + lat + ',' + lon + '&key=' + apiKey).done(function (response) {
-        if (response.results.length > 0) {
+                var results = response.results.filter(result => {
+                    return result.type === "address"
+                })
 
-            var results = response.results.filter(result => {
-                return result.type === "address"
-            })
-
-            if(results.length > 1){
-                results = results.filter(l => {
-                    return l.location_type === "interpolated"
-                });
-            }
-            else {
-                if (results.length == 0) {
-                    results = response.results.filter(result => {
-                        return result.type === "road" && result.components[0].value != "(Unnamed Road)"
+                if(results.length > 1){
+                    results = results.filter(l => {
+                        return l.location_type === "interpolated"
                     });
+                }
+                else {
                     if (results.length == 0) {
                         results = response.results.filter(result => {
-                            return result.type === "locality"
+                            return result.type === "road" && result.components[0].value != "(Unnamed Road)"
                         });
+                        if (results.length == 0) {
+                            results = response.results.filter(result => {
+                                return result.type === "locality"
+                            });
+                        }
                     }
                 }
+
+                results.forEach(function(element){
+                    var formattedResult =  element.formatted_result;
+
+                    popup = L.popup()
+                        .setLatLng([lat,lon])
+                        .setContent(formattedResult)
+                        .openOn(map);
+                });
+            } else {
+                alert("NO_RESULTS");
             }
+        });
+    }
 
-            results.forEach(function(element){
-                var formattedResult =  element.formatted_result;
-
-                popup = L.popup()
-                    .setLatLng([lat,lon])
-                    .setContent(formattedResult)
-                    .openOn(map);
-            });
-        } else {
-            alert("NO_RESULTS");
-        }
-    });
+    whatIsHere(lat,lon);
 }
