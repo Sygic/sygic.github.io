01f8744c19f1a4bd35d637ca0beb549b79c1159f
tomas0620 <tomas0620@users.noreply.github.com>
2018-11-07 09:32:30 +0100

    fixed send to navigation (#9)
    * fixed send to navigation
    
    * simplify demo
    
    * fix duplicate call sendToNavigation()
    
    * fix README

--- send-to-navi-demo/script.js
+++ send-to-navi-demo/script.js
@@ -1,100 +1,84 @@
 window.onload = function () {
     var mapApiKey = getApiKey();
-
-    var map = L.map("map");
-
-    var encodedPolylineString = "u`{dHqjlgBARJjB{@Jy@F_BJg@VEJ?dCaAnDbAt@tA\\bC`@X}EV_EFq@b@CL?V?H?d@?n@LPVXr@n@|AfCjFDH\\\\FFV\\\\b@h@b@VNPHd@T~AVvABN?xHo@~Ci@z@KPA`@Af@Af@AdBM|ACJAd@CT?\\CV@T?T?xEAlGKp@A\\A`AAxDGpEIbACtFIdACR?RA^?\\?n@AXAXAnAC\\NTTRh@A~AWXcB~B[@a@UK]I_A@_ADqD@aA@YN}M@q@?O@YR{LFwDDuD?c@L}IHmGDoD@u@@WHeHD_EFqDHsFPmKHwCVqRF{DBkCBsC@cA@KA{DGsBWiEc@kDw@eEOk@IWy@aCy@kBgAqBKUa@}@aDmFGIKOkEqG_E}FoBuCsAqBy@kAg@s@cG{I[e@Yc@c@m@kAeBeDyE_C_EsAcDcAeDkAqG[yCYyGQk[CeC?sAGeOEiJEmKGkQAeAIuLY{Hg@sF[uB_BsHeAgD{AqDo@mAcD{EiCkCiDcCaBw@m@WcD{@}De@oAKaAKGAgJs@iBMgE[{AMm@EkDWwXwBwCW}@K]EmAMwC]aG{@IAWEaAQyHyAuG_Bu@Q_EgA{KoDmHoCiHcDyF}CqEqCgI}FeK}I{@{@mF{FEEOOuHgJoCuDcF_Ia@q@kD{FmDyGkDqHKUsIsSkEeL[w@yAwD}@}BeHaRqGsPqImTKUWk@eAkCm@yAc@aAaB{Dk@sAwF}LqFgLoOgZuG}M{D}ImCmGuG}PaGgQ}CeKqDoMqIs^cFcXiEaYgBmNcAmJiAoLm@{GiB{Xi@wKgAm\\g@{c@?uc@`Ao{A?m_@c@}[o@uQy@}NyAeQ_A{IsB}OaDqR_DkO_GkUqAiEgEkMkEgLaIwQeCeF}JqReVec@Q[{KoSmA}Bi@aAoH_OaAsBqIkRsBcFqAcD{@}BsDeKcEoMoEcPsHa\\_A_F{CgQkCiRa@eDmAoK[_DkAiNeBkWcAaSsCko@c@}IM{Bm@qJsAmQeAaLeBmOiBwM{BeNkCsN_CwKoFkTuAaFoEuNkFgO}ImTmKkTwJuPcJ}MsGuIiIuJoJkKo]w^uCeDqFqGoBaCeB}BKMwBoCiJiMsAoB}MqSwGwK{LwTuHgOaKwT{Ma\\_F{M_GwPcGkRsDcMcFeRiDcNeIq^iHw_@oDuTwEu\\gC_TaB_PoHyx@iCwVaFcc@_@sCqDwYc@oCy@wGoD{U{G{`@{D{TiL{k@kF_VwJ_b@wN_j@aHcV}Okg@K[wLs]]aAsAwD{ImUUm@q@mBkKoWoKuVgIcQu@_BkAeCeEwIMUSa@iA_CwAoC{CgGeAsBsB{DwAuCMu@Wo@UwA@uATs@X_@`@WTCh@Hb@^Zn@LfAEz@Qx@W`@w@j@Y`@QFGBc@Pe@P_@NuFxBmD`BuCxAsBjAk@^iDzByFhE{GhGiDlDyF|GmBpCaClD}C|E{CvF{ChGeEpJmKnXiFrKeAhBwBhD_ApAs@`AyDtEqBrBq@n@s@p@WT}@t@aDdCYP{BvA}A|@aAf@o@Z]PSLuAv@_@Pk@ZsAv@mC~AmAt@sCdBmDvBu@p@kAtAi@v@eAvBKXWp@s@rBu@hAQAWNmAe@qAc@_A_@mBQq@NaCdAWR{AbB_@d@u@rA}@`Bi@~@u@z@eBx@{ARQ?_@B]Da@D{AH]BM@m@FcBNiHfAoKzB_B`@QDUFu@RkRnIq@TBXJdADj@DZBZP~A`@jDf@~FV`D";
-
-    var polyline = L.Polyline.fromEncoded(encodedPolylineString, {
-        color: 'blue',
-        weight: 3,
-        smoothFactor: 1
-    });
-
-    var coords = polyline.getLatLngs();
-
-    var startMarker = L.marker(coords[0], { title: "Start" });
-    var endMarker = L.marker(coords[coords.length - 1], { title: "End" });
+    var map = L.map("map").setView([44.67646564865964, 10.625152587890625], 13);
 
     var sygicTileLayer = L.TileLayer.sygic(mapApiKey);
-    L.layerGroup([sygicTileLayer, polyline, startMarker, endMarker]).addTo(map);
+    L.layerGroup([sygicTileLayer]).addTo(map);
 
-    var bounds = new L.LatLngBounds(coords);
-    map.fitBounds(bounds);
+    var request = {
+        "destination": "44.67646564865964,10.625152587890625",
+        "origin": "44.65529852148082,10.880284309387207"
+    };
 
-    L.easyButton('fa-send-o',
-        run,
-        'Send route to navigation'
-    ).addTo(map);
-}
+    //call sygic directions api
+    $.ajax({
+        url: "https://routing.api.sygic.com/v0/api/directions?key=" + mapApiKey,
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        data: JSON.stringify(request)
+    }).done(function (response) {
+        if (response.routes.length > 0) {
+            var route = response.routes[0];
 
-function run() {
-    var apiKey = prompt("Please enter ApiKey for Send route to navigation API");
+            //create leaflet polyline object from api response
+            var polyline = L.Polyline.fromEncoded(route.route, {
+                color: 'blue',
+                weight: 3,
+                smoothFactor: 1
+            });
+            var coords = polyline.getLatLngs();
 
-    if (apiKey) {
-        var sendToNaviUrl = "https://directions.api.sygic.com/v0/api/sendtonavi?key=" + apiKey;
+            var startMarker = L.marker(coords[0], { title: "Start" });
+            var endMarker = L.marker(coords[coords.length - 1], { title: "End" });
+            L.layerGroup([polyline, startMarker, endMarker]).addTo(map);
 
-        var tag = prompt("Please enter login or IMEI of the device in format \"login_YourLogin@email.com\" or \"id_IMEIOfDevice\"");
-        if (tag) {
+            var bounds = new L.LatLngBounds(coords);
+            map.fitBounds(bounds);
 
-            var sendToNaviApiInput = {
-                message: "This route was send from demo.",
-                name: "Demo route",
-                tags: tag,
-                directions_api_parameters: {
-                    origin: "48.148540,17.107752",
-                    destination: "48.382242,17.586862"
-                },
-                directions_api_result: {
-                    route: "u`\{dHqjlgBARJjB{@Jy@F_BJg@VEJ?dCaAnDbAt@tA\\bC`@X}EV_EFq@b@CL?V?H?d@?n@LPVXr@n@|AfCjFDH\\\\FFV\\\\b@h@b@h@X`A^bALvABN?xHo@zEu@PA`@Af@Af@A|@Gf@E|ACJAd@CT?\\CV@T?T?xEA|IO`AAxDGpEIbACtFIdACR?RA^?\\?n@AXAXAnAC\\NTTRh@A~AWXcB~B[@a@UK]I_A@_ADqD@aA@YN}M@q@?O@YR\{LFwDDuD?c@L}IHmGDoD@u@@WHeHD_EFqDHsFPmKHwCVqRF\{DBkCBsC@cA@KA{DGsBWiEc@kDw@eEOk@IWy@aCy@kBgAqBKUa@}@aDmFGIKOkEqG_E}FoBuCsAqBy@kAg@s@cG\{I[e@Yc@c@m@kAeBeDyE_C_EsAcDcAeDkAqG[yCYyGQk[CeC?sAGeOEiJEmKIqSIuLY{Hg@sF[uB_BsHeAgD{AqDo@mAcD{EiCkCiDcCaBw@m@WcD{@}De@oAKaAKGAgJs@iBMgE[\{AMm@EkDWwXwBwCW}@K]EmAMwC]KAuFy@a@G\{JkBuG_Bu@Q_EgA{KoDmHoCiHcDyF}CqEqCgI}FeK}I\{@{@mF{FEEOOuHgJoCuDcF_Ia@q@kD{FmDyGkDqHKUsIsSkEeL[w@yAwD}@}BeHaRqGsPqImTKUWk@eAkCm@yAc@aAaB\{Dk@sAwF}LqFgLoOgZuG}M\{D}ImCmGuG}PaGgQ}CeKqDoMqIs^cFcXiEaYgBmNm@oFU}BiAoLm@\{GiB{Xi@wKgAm\\g@{c@?uc@`Ao{A?m_@c@}[o@uQy@}NyAeQ_A\{IsB}OaDqR_DkO_GkUqAiEgEkMkEgLaIwQeCeF}JqReVec@Q[\{KoSmA}Bi@aAoH_OaAsBqIkRsBcFqAcD\{@}BsDeKcEoMoEcPsHa\\_A_F\{CgQkCiRa@eDmAoK[_DkAiNiBkXe@}ImDow@oAuU}@oM\{BkVuAwLiBwM{BeNkCsN_CwKoFkTuAaFoEuNkFgO}ImTmKkTwJuPcJ}MsGuIiIuJoJkKo]w^uCeDqFqGuE_GKMwBoCiJiMsAoB}MqSwGwK\{LwTuHgOaKwT{Ma\\_F{M_GwPcGkRsDcMcFeRiDcNeIq^iHw_@oDuTwEu\\gC_TaB_PoHyx@iCwVaFcc@_@sCqDwYc@oCy@wGoD{U{G{`@{D{TiL{k@kF_VwJ_b@wN_j@aHcV}Okg@K[wLs]]aAsAwD\{ImUUm@q@mBkKoWoKuVgIcQu@_BkAeCeEwIMUSa@iA_CwAoC{CgGeAsBsB{DwAuCMu@Wo@UwA@uATs@X_@`@WTCh@Hb@^Zn@LfAEz@Qx@W`@w@j@Y`@QFGBc@Pe@P_@NuFxBmD`BuCxAsBjAk@^iDzByFhE{GhGiDlDyF|GmBpCaClD}C|E\{CvF{ChGeEpJmKnXiFrKeAhBwBhD_ApAs@`AyDtEqBrBq@n@s@p@WT}@t@aDdCYP\{BvA}A|@aAf@o@Z]PSLuAv@_@Pk@ZsAv@mC~AmAt@sCdBmDvBu@p@kAtAi@v@eAvBKXWp@s@rBu@hAQAWNmAe@qAc@_A_@mBQq@NaCdAWR\{AbB_@d@u@rA}@`Bi@~@u@z@eBx@{ARQ?_@B]Da@D{AH]BM@m@FcBNiHfAoKzB_B`@QDUFu@RkRnIq@TBXJdADj@DZBZP~A`@jDf@~FV`D",
-                    eta: 1520251289,
-                    duration: {
-                        value: 2512,
-                        text: "0 hours 41 minutes 52 seconds"
-                    },
-                    distance: {
-                        value: 58008,
-                        text: "58.01 km"
-                    },
-                    legs: [
-                        {
-                            distance: {
-                                value: 58008,
-                                text: "58.01 km"
-                            },
-                            duration: {
-                                value: 2512,
-                                text: "0 hours 41 minutes 52 seconds"
-                            },
-                            start_location: {
-                                latitude: 48.14875,
-                                longitude: 17.10777
-                            },
-                            end_location: {
-                                latitude: 48.38213,
-                                longitude: 17.58687
-                            },
-                            route: "u`\{dHqjlgBARJjB{@Jy@F_BJg@VEJ?dCaAnDbAt@tA\\bC`@X}EV_EFq@b@CL?V?H?d@?n@LPVXr@n@|AfCjFDH\\\\FFV\\\\b@h@b@h@X`A^bALvABN?xHo@zEu@PA`@Af@Af@A|@Gf@E|ACJAd@CT?\\CV@T?T?xEA|IO`AAxDGpEIbACtFIdACR?RA^?\\?n@AXAXAnAC\\NTTRh@A~AWXcB~B[@a@UK]I_A@_ADqD@aA@YN}M@q@?O@YR\{LFwDDuD?c@L}IHmGDoD@u@@WHeHD_EFqDHsFPmKHwCVqRF\{DBkCBsC@cA@KA{DGsBWiEc@kDw@eEOk@IWy@aCy@kBgAqBKUa@}@aDmFGIKOkEqG_E}FoBuCsAqBy@kAg@s@cG\{I[e@Yc@c@m@kAeBeDyE_C_EsAcDcAeDkAqG[yCYyGQk[CeC?sAGeOEiJEmKIqSIuLY{Hg@sF[uB_BsHeAgD{AqDo@mAcD{EiCkCiDcCaBw@m@WcD{@}De@oAKaAKGAgJs@iBMgE[\{AMm@EkDWwXwBwCW}@K]EmAMwC]KAuFy@a@G\{JkBuG_Bu@Q_EgA{KoDmHoCiHcDyF}CqEqCgI}FeK}I\{@{@mF{FEEOOuHgJoCuDcF_Ia@q@kD{FmDyGkDqHKUsIsSkEeL[w@yAwD}@}BeHaRqGsPqImTKUWk@eAkCm@yAc@aAaB\{Dk@sAwF}LqFgLoOgZuG}M\{D}ImCmGuG}PaGgQ}CeKqDoMqIs^cFcXiEaYgBmNm@oFU}BiAoLm@\{GiB{Xi@wKgAm\\g@{c@?uc@`Ao{A?m_@c@}[o@uQy@}NyAeQ_A\{IsB}OaDqR_DkO_GkUqAiEgEkMkEgLaIwQeCeF}JqReVec@Q[\{KoSmA}Bi@aAoH_OaAsBqIkRsBcFqAcD\{@}BsDeKcEoMoEcPsHa\\_A_F\{CgQkCiRa@eDmAoK[_DkAiNiBkXe@}ImDow@oAuU}@oM\{BkVuAwLiBwM{BeNkCsN_CwKoFkTuAaFoEuNkFgO}ImTmKkTwJuPcJ}MsGuIiIuJoJkKo]w^uCeDqFqGuE_GKMwBoCiJiMsAoB}MqSwGwK\{LwTuHgOaKwT{Ma\\_F{M_GwPcGkRsDcMcFeRiDcNeIq^iHw_@oDuTwEu\\gC_TaB_PoHyx@iCwVaFcc@_@sCqDwYc@oCy@wGoD{U{G{`@{D{TiL{k@kF_VwJ_b@wN_j@aHcV}Okg@K[wLs]]aAsAwD\{ImUUm@q@mBkKoWoKuVgIcQu@_BkAeCeEwIMUSa@iA_CwAoC{CgGeAsBsB{DwAuCMu@Wo@UwA@uATs@X_@`@WTCh@Hb@^Zn@LfAEz@Qx@W`@w@j@Y`@QFGBc@Pe@P_@NuFxBmD`BuCxAsBjAk@^iDzByFhE{GhGiDlDyF|GmBpCaClD}C|E\{CvF{ChGeEpJmKnXiFrKeAhBwBhD_ApAs@`AyDtEqBrBq@n@s@p@WT}@t@aDdCYP\{BvA}A|@aAf@o@Z]PSLuAv@_@Pk@ZsAv@mC~AmAt@sCdBmDvBu@p@kAtAi@v@eAvBKXWp@s@rBu@hAQAWNmAe@qAc@_A_@mBQq@NaCdAWR\{AbB_@d@u@rA}@`Bi@~@u@z@eBx@{ARQ?_@B]Da@D{AH]BM@m@FcBNiHfAoKzB_B`@QDUFu@RkRnIq@TBXJdADj@DZBZP~A`@jDf@~FV`D"
-                        }
-                    ]
-                }
-            }
-            send(sendToNaviApiInput);
+            // Fired when the grid layer loaded all visible tiles.
+            sygicTileLayer.once('load', function() {
+                sendToNavigation(route);
+            });
+
+        } else {
+            alert("NO_RESULTS");
         }
-    }
+    });
 
-    // post request for send to navigation
-    function send(data) {
-        $.ajax({
-            type: 'POST',
-            url: sendToNaviUrl,
-            contentType: "application/json; charset=UTF-8",
-            data: JSON.stringify(data),
-        }).done(function (data, textStatus, xhr) {
-            // get result of send route to navi
-            alert("Status: " + textStatus)
-        }).fail(function (data, textStatus, xhr) {
-            alert("FAILED!\r\n" + JSON.stringify(data, null, 2));
-        });
+    function sendToNavigation(route) {
+        var apiKey = prompt("Please enter ApiKey for Send route to navigation API");
+
+        if (apiKey) {
+            var sendToNaviUrl = "https://directions.api.sygic.com/v0/api/sendtonavi?key=" + apiKey;
+
+            var tag = prompt("Please enter login or IMEI of the device in format \"login_YourLogin@email.com\" or \"id_IMEIOfDevice\"");
+            if (tag) {
+
+                var sendToNaviApiInput = {
+                    message: "This route was send from demo.",
+                    name: "Demo route",
+                    tags: tag,
+                    directions_api_parameters: request,
+                    directions_api_result: route
+                };
+                send(sendToNaviApiInput);
+            }
+        }
+
+        // post request for send to navigation
+        function send(data) {
+            $.ajax({
+                type: 'POST',
+                url: sendToNaviUrl,
+                contentType: "application/json; charset=UTF-8",
+                data: JSON.stringify(data),
+            }).done(function (data, textStatus, xhr) {
+                // get result of send route to navi
+                alert("Status: " + textStatus)
+            }).fail(function (data, textStatus, xhr) {
+                alert("FAILED!\r\n" + JSON.stringify(data, null, 2));
+            });
+        }
     }
 }
