03f9fad21f92c1fff7c5adcbfeca915361dbd85c
Tomas Cicman <tcicman@sygic.com>
2018-11-29 15:18:51 +0100

    regenerate documentation demo of maps layers

--- documentation-demos/maps-layers-demo/demo.js
+++ documentation-demos/maps-layers-demo/demo.js
@@ -1,32 +1,34 @@
 window.onload = function () {
-  var apiKey = getApiKey();
+    var apiKey = prompt("Please ender an API key valid for TomTom maps.");
 
-  var map = L.map("map", {
-      center: [51.500434, -0.12634],
-      zoom: 15
-  });
+    if (apiKey == null || apiKey == "") {
+        return location.reload();
+    }
+    var map = L.map("map", {
+        center: [51.500434, -0.12634],
+        zoom: 15
+    });
 
+    var mapLayerUser = L.TileLayer.sygic(apiKey, {detectRetina: false});
+    var sateliteLayer = L.TileLayer.sygicSatellite(apiKey, {detectRetina: false});
+    var poiLayer = L.TileLayer.sygicPoi(apiKey);
+    var restrictionLayer = L.TileLayer.sygicRestriction(apiKey);
+    var incidentLayer = L.TileLayer.sygicIncident(apiKey);
 
-  var mapLayerUser = L.TileLayer.sygic(apiKey, { detectRetina: false });
-  var sateliteLayer = L.TileLayer.sygicSatellite(apiKey, { detectRetina: false });
-  var poiLayer = L.TileLayer.sygicPoi(apiKey);
-  var restrictionLayer = L.TileLayer.sygicRestriction(apiKey);
-  var incidentLayer = L.TileLayer.sygicIncident(apiKey);
+    var trafficLayer = new L.TileLayer.sygicTraffic(apiKey, {
+        trafficControl: true
+    });
 
-  var trafficLayer = new L.TileLayer.sygicTraffic(apiKey, {
-      trafficControl: true
-  });
+    var baseMaps = {'Map': mapLayerUser, 'Satelite': sateliteLayer};
 
-  var baseMaps = { 'Map': mapLayerUser, 'Satelite': sateliteLayer };
+    var overlayMaps = {
+        'Poi': poiLayer,
+        'Restriction': restrictionLayer,
+        'Traffic': trafficLayer,
+        'Incident': incidentLayer
+    };
 
-  var overlayMaps = {
-      'Poi': poiLayer,
-      'Restriction': restrictionLayer,
-      'Traffic': trafficLayer,
-      'Incident': incidentLayer
-  };
+    L.layerGroup([mapLayerUser, poiLayer, restrictionLayer, incidentLayer, trafficLayer]).addTo(map);
 
-  L.layerGroup([mapLayerUser, poiLayer, restrictionLayer, incidentLayer, trafficLayer]).addTo(map);
-
-  L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+    L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(map);
 }
