7f7c36cf8b5ac3ea225d2ff5466ee63d847e60f3
Jozef Lacny <jlacny@sygic.com>
2019-03-07 16:34:17 +0100

    added more interaction to directions-live-demo

--- documentation-demos/directions-live-demo/demo.js
+++ documentation-demos/directions-live-demo/demo.js
@@ -1,18 +1,51 @@
 window.onload = function () {
     let apiKey = getApiKey();
 
+    /* initialize map */
     let map = L.map("map");
+    let latLngClicked;
+
+    map.on('contextmenu', function(e,b) {
+        L.popup().setLatLng(e.latlng)
+        .setContent("<ul class='context-menu'>" + 
+                        "<li><a id='from-here'><i class='fa fa-flag'></i> Directions from here</a></li>"+
+                        "<li><a id='to-here'><i class='fa fa-flag-checkered'></i> Directions to here</a></li>"+
+                    "</ul>").openOn(map);
+        latLngClicked = e.latlng;         
+        tempAlert(coordsToString(e.latlng));
+    });
 
     let sygicTileLayer = L.TileLayer.sygic(apiKey);
-    let responseLayer;
-    L.layerGroup([sygicTileLayer]).addTo(map);
+    let incidentLayer = L.TileLayer.sygicIncident(apiKey);
+    let restrictionLayer = L.TileLayer.sygicRestriction(apiKey);
+    let sateliteLayer = L.TileLayer.sygicSatellite(apiKey, { detectRetina: false });
+    let trafficLayer = new L.TileLayer.sygicTraffic(apiKey, {
+        trafficControl: true
+    });
+    let poiLayer = L.TileLayer.sygicPoi(apiKey);
+    let baseMaps = { 'Map': sygicTileLayer, 'Satelite': sateliteLayer };
 
+    let overlayMaps = {
+        'Poi': poiLayer,
+        'Restriction': restrictionLayer,
+        'Traffic': trafficLayer,
+        'Incident': incidentLayer
+    };
+
+    L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+
+    L.layerGroup([sygicTileLayer, incidentLayer, poiLayer, restrictionLayer]).addTo(map);
+
+    let responseLayer;
+
+    /* REQUEST BODY */
     let request = {
         origin: "48.1464,17.10687",
         destination: "49.00254,21.23971",
         compute_alternatives: true
     };
     
+    /* call directions api, display on map */
     let compute = function (request) {
         //call sygic directions api
         $.post('https://routing.api.sygic.com/v0/api/directions?key=' + apiKey, request).done(function (response) {
@@ -30,7 +63,7 @@ window.onload = function () {
                         color: i == 0 ? 'purple' : 'gray',
                         weight: 5,
                         smoothFactor: 1,
-                        opacity: i == 0 ? 1 : 0.5
+                        opacity: i == 0 ? 1 : 0.7
                     }).addTo(responseLayer);                    
                     let coords = polyline.getLatLngs();
 
@@ -50,16 +83,51 @@ window.onload = function () {
 
     compute(request);
 
+    /* utils */
     let createMarker = function (coords, title) {
         let marker = L.marker(coords, {
             title: title,
             draggable: true
         });
         marker.on('dragend', function (evt) {
-            let newCoords = evt.target.getLatLng().lat.toFixed(5) + "," + evt.target.getLatLng().lng.toFixed(5);
+            let newCoords = coordsToString(evt.target.getLatLng());
             request[evt.target.options.title] = newCoords;
             compute(request);
         });
         return marker;
+    };
+
+    let tempAlert = function (msg, color, duration) {
+        duration = duration || 2000;
+        color = color || "lightgray";
+        var el = document.createElement("div");
+        el.setAttribute("style", `position:absolute;top:5%;left:50%;background-color:${color};z-index:999;padding:5px`);
+        el.innerHTML = msg;
+        setTimeout(function () {
+            el.parentNode.removeChild(el);
+        }, duration);
+        document.body.appendChild(el);
+    };
+
+    $('body').on('click', '#from-here', function() {
+        map.closePopup();
+        tempAlert(coordsToString(latLngClicked));
+        $.extend(request, {
+            origin: coordsToString(latLngClicked)
+        });
+        compute(request);
+    });
+
+    $('body').on('click', '#to-here', function() {
+        map.closePopup();
+        tempAlert(coordsToString(latLngClicked));
+        $.extend(request, {
+            destination: coordsToString(latLngClicked)
+        });
+        compute(request);
+    });
+
+    let coordsToString = function (latlng) {
+        return latlng.lat.toFixed(5) + "," + latlng.lng.toFixed(5);
     };
 };
