7ddedf979ad26b34cb291d2fb9af5f0b8bdbbfa5
kvimbi <tomas@michalek.it>
2019-08-26 22:30:41 +0200

    Fix reactjs resource url

---
+++ sdk_config/precache-manifest.fda3f57612806dc6d90e345a68decccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fc96f8b8e6572644e030a5ddec4f36",
+    "url": "/sdk_config/index.html"
+  },
+  {
+    "revision": "42bee01a45cdefe3755f",
+    "url": "/sdk_config/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "7785597c63e1a610b76a",
+    "url": "/sdk_config/static/css/main.0fcc318e.chunk.css"
+  },
+  {
+    "revision": "42bee01a45cdefe3755f",
+    "url": "/sdk_config/static/js/2.898c1e13.chunk.js"
+  },
+  {
+    "revision": "7785597c63e1a610b76a",
+    "url": "/sdk_config/static/js/main.8a27388b.chunk.js"
+  },
+  {
+    "revision": "87890f1f9b24d49804d0",
+    "url": "/sdk_config/static/js/runtime~main.090b0881.js"
+  }
+]);
